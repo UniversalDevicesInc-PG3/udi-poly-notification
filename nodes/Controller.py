9684ad577ae5112cc87b4186daa7ecdcdafd4a56
JimBo <jimboca3@gmail.com>
2019-02-05 10:53:51 -0800

    Update Controller.py

--- nodes/Controller.py
+++ nodes/Controller.py
@@ -273,7 +273,7 @@ class Controller(polyinterface.Controller):
             if not node in self.nodes:
                 self.l_error('rest_handler', 'unknown node "{}"'.format(node))
                 return False
-            self.nodes[node].rest_send(params,data.decode)
+            self.nodes[node].rest_send(params,data.decode())
         return True
 
     """
